a418590b713bea5a047700832fa2ed54ee89aa2d
floconno <floconno@asu.edu>
2024-03-23 09:21:51 -0700

    created file structure, made inquirer prompt, created shape class and response

---
+++ index.js
@@ -0,0 +1,55 @@
+
+const inquirer = require('inquirer');
+const {Triangle} = require("./lib/shapes");
+
+
+function menu() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'What is the text for your logo?', 
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'What is the text color for your logo?', 
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Select the shape for your logo',
+            choices: [
+                "circle",
+                "square",
+                "triangle"
+            ] 
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'What is the shape color for your logo?', 
+        },
+    ])
+    .then(response => {
+        console.log(response);
+        let shape;
+        if(response.shape === "triangle") {
+            shape = new Triangle
+        }
+        shape.setColor(response.shapeColor)
+
+        const svg = `
+        <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+        ${shape.render()}
+
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${response.textColor}">${response.text}</text>
+
+        </svg>
+        `
+        console.log(svg);
+    }) 
+}
+
+menu();
